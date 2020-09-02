21ec689c62ec5510a98a59816bbd99a144db036a
cjd6568358 <cjd6568358@163.com>
2020-09-03 00:31:03 +0800

    fix

--- pages/children/map.js
+++ pages/children/map.js
@@ -6,18 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    mapSetting: {
-      id: "map",
-      scale: 14,
-      showLocation: true,
-      enableZoom: true,
-      enableScroll: true,
-      enableRotate: false,
-      showCompass: false,
-      longitude: "113.324520",
-      latitude: "23.099994",
-      markers: []
-    },
+    markers: [],
+    includePoints: [],
     fieldList: []
   },
 
@@ -25,23 +15,43 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setNavigationBarTitle({
-      title: options.title,
-    })
+
     //获取事件对象
     const eventChannel = this.getOpenerEventChannel()
     // 监听acceptData事件，获取上一页面通过eventChannel传送到当前页面的数据
-    eventChannel.on('content', (info) => {
+    eventChannel.on('content', ({ name, location, fieldList }) => {
+      wx.setNavigationBarTitle({
+        title: name,
+      })
+
+      let [latitude, longitude] = location.split(',')
       this.setData({
-        mapSetting: {
-          longitude: info.location.lon,
-          latitude: info.location.lat,
-          markers: [{
-            longitude: info.location.lon,
-            latitude: info.location.lat,
-          }]
-        },
-        fieldList: info.fieldList
+        longitude,
+        latitude,
+        markers: [{
+          longitude,
+          latitude,
+          id: 1,
+          callout: {
+            content: name,
+            display: 'ALWAYS',
+            padding: 10,
+            borderRadius: 5
+          }
+        }],
+        fieldList: fieldList
+      }, () => {
+        wx.getLocation({
+          type: 'gcj02',
+          success: (res) => {
+            const latitude = res.latitude
+            const longitude = res.longitude
+            this.setData({
+              includePoints: [...this.data.markers, { latitude, longitude }],
+              polyline: [{ points: [...this.data.markers, { latitude, longitude }] }],
+            })
+          }
+        })
       })
     })
   },
@@ -58,6 +68,9 @@ Page({
         phoneNumber: item.value,
       })
     }
+  },
+  markertap(e) {
+    console.log(e.detail.markerId)
   },
   /**
    * 生命周期函数--监听页面显示
